da33d7c93baa9bb55dfadfb6b5e424bf640c38d1
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2019-01-08 01:28:37 +0300

    fix shuggle options bug

--- types/choose_options/index.js
+++ types/choose_options/index.js
@@ -1,26 +1,25 @@
-const randomSort = require('../../utils/randomSort');
 const getUniqueId = require('../../utils/getUniqueId');
 
 module.exports = ({ card, tags }) => {
   const uniqId = getUniqueId(card.question);
-  const questionsHtml = randomSort(card.answers)
-    .map(
-      el =>
-        `<label  class="is_not_checked should_be_${
-          el.correct ? '' : 'un'
-        }checked"><input type="checkbox" /> <span class="question-text">${
-          el.text
-        }</span></label>`
-    )
-    .join('<br />');
 
   const front = `
   <div id="${uniqId}_wrapper">
     <p>${card.question}</p>
-    <div class="questions-wrapper">${questionsHtml}</div>
+    <div class="questions-wrapper"></div>
   </div>
   <button id="${uniqId}_checkBtn">Check</button>
   <script>
+    var questionContainer = document.querySelector('.questions-wrapper');
+    var cardAnswers = ${JSON.stringify(card.answers)};
+    var answersHTML = cardAnswers
+      .sort(el => Math.random() - 0.5)
+      .map(el => \`<label class="is_not_checked should_be_\${ el.correct ? 'checked' : 'unchecked' }">
+        <input type="checkbox"/>
+        <span class="question-text">\${ el.text }</span></label>\`)
+      .join('<br />');
+
+    questionContainer.innerHTML = answersHTML;
     var contentWrapper = document.querySelector('#${uniqId}_wrapper');
     var checkBtn = document.querySelector('#${uniqId}_checkBtn');
     checkBtn.addEventListener('click', function() { contentWrapper.classList.add('checked'); });

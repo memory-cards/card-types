d61bad79eb1ec1157cc1c63cc29030d52964a776
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2019-01-23 21:18:40 +0300

    Add schema and html markdown

---
+++ types/choose_sequence/validator.js
@@ -0,0 +1,13 @@
+require('json5/lib/register');
+const { Validator } = require('jsonschema');
+
+const schema = require('./json-schema.json');
+
+const v = new Validator();
+
+/**
+ * Check if passed object is valid or not
+ * @param obj {object}
+ * @returns {boolean}
+ */
+module.exports = obj => v.validate(obj, schema).errors.length === 0;

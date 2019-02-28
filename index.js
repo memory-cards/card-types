2a0f33b12ee49170a7e7dac20bd4627deecf5cd8
Evgeniy Dainovich <zenbysel@gmail.com>
2019-02-28 20:15:00 +0300

    add choose_sequence interface

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cardIndex = require('./types/choose_options/index');
 const infoIndex = require('./types/info/index');
 const chooseOptionsIndex = require('./types/choose_options/index');
+const chooseSequenceIndex = require('./types/choose_sequence/index');
 
 module.exports = {
   choose_options: {
@@ -11,5 +12,8 @@ module.exports = {
   },
   order_items: {
     card: chooseOptionsIndex,
+  },
+  choose_sequence: {
+    card: chooseSequenceIndex,
   },
 };

13d1c3a5d0efecbc99d5003366f70f2a98e6d3bb
zenby <32563455+zenby@users.noreply.github.com>
2019-03-31 14:20:24 +0300

    Update <code> style & restrict uniqueId length (#18)

--- utils/getUniqueId.js
+++ utils/getUniqueId.js
@@ -1,4 +1,5 @@
 module.exports = (prefix = 'xxx') =>
   Buffer.from(`${prefix}-${Date.now()}-${Math.random()}`)
     .toString('base64')
-    .replace(/[^a-zA-Z]/g, '');
+    .replace(/[^a-zA-Z]/g, '')
+    .substring(0, 30);
